921019f540bf55e6164b85b9855701bb97b3fc65
Abinash <abixnash@gmail.com>
2025-12-19 22:46:25 +0530

    Added dot env related dependency

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
+	godotenv.Load()
 	fmt.Println("Hello there!")
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("PORT is not defined.")
+	}
+
+	fmt.Println("Application is running on", port)
 }
